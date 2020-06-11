04570fb278bfe8a6507c3166c34dccc6f6df1072
mohamed amine boughdir <1933@holbertonschool.com>
2020-06-11 12:31:01 +0000

    %10

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -12,8 +12,7 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 {
-printf("Last digit of %d\n", n % 10);
-printf("is %d ", n % 10);
+printf("Last digit of %d is %d\n", n % 10);
 }
 if ((n % 10) > 5)
 {
