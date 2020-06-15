8efe1d311346f5815062d726c48302e38029beba
mohamed amine boughdir <1933@holbertonschool.com>
2020-06-15 14:40:25 +0000

    try

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * main -putchar
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+char x;
+
+for (x = 'a'; x <= 'z'; x++)
+{
+_putchar(x);
+}
+_putchar('\n');
+return ;
+}
