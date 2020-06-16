fd136c5ff952326c5eb5fd021a96614c01b5e033
amineboughedir <66303969+amineboughedir@users.noreply.github.com>
2020-06-16 14:21:13 +0000

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,28 +1,22 @@
-B#include "holberton.h"
+#include "holberton.h"
 /**
- *print_sign- function that prints the sign of a number.
- *@c: int
- *Return: 1 or -1 or 0
- */
+*print_sign - function that computes the absolute value 
+*@n: integer
+*Return: 1 , 0 or -1
+*/
 int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
 
-{
-if (n > 0)
-{
-_putchar('+');
-return (1);
-}
-else if
-{
-(n = 0)
-{
-_putchar('0');
-return ('0');
-}
-else
-{
-(n < 0)
-_putchar('-');
-return (-1);
-}
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	_putchar('0');
+	return (0);
 }
