20034d9a73e58dc5637e12dffa4d6fca789a7980
mohamed amine boughdir <1933@holbertonschool.com>
2020-06-18 12:55:29 +0000

    h

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,3 +1,6 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+int _putchar(int);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -8,3 +11,4 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+#endif
