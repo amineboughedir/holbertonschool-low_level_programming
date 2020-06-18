eadcc280c1cdb38aa2c81d000a027c42c03b7c01
mohamed amine boughdir <1933@holbertonschool.com>
2020-06-18 11:07:19 +0000

    tt

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *print_numbers - function that prints the numbers .
+ */
+void print_numbers(void)
+{
+char x;
+
+for (x = '0'; x <= '9'; x++)
+{
+_putchar(x);
+}
+_putchar('\n');
+}
